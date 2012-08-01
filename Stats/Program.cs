3193a0ffc905e7534014d293d19db29ce56da54f
Victor Grigoriu <vgrigoriu@gmail.com>
2012-08-01 12:33:33 +0300

    Gino's version of observable which actually works

--- Stats/Program.cs
+++ Stats/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Reactive.Linq;
 using System.Reactive.Disposables;
+using System.Reactive.Subjects;
+using System.Reactive.Concurrency;
 
 namespace Stats
 {
@@ -11,25 +13,33 @@ namespace Stats
     {
         static void Main(string[] args)
         {
-            var data = Observable.Create<double>(observer =>
-            {
-                while (true)
-                {
-                    var line = Console.ReadLine();
-                    double value;
-                    if (double.TryParse(line, out value))
-                    {
-                        observer.OnNext(value);
-                    }
-                }
-
-                return Disposable.Empty;
-            });
+            var data = ConsoleToObservable();
 
             data.Subscribe(x => Console.WriteLine("value: {0}", x));
 
             var totals = data.Scan(0.0, (total, x) => total + x);
             totals.Subscribe(total => Console.WriteLine("total: {0}", total));
+
+            data.Connect();
+        }
+
+        static IConnectableObservable<double> ConsoleToObservable()
+        {
+            return Observable.Create<double>(observer =>
+            {
+                return Scheduler.Immediate.Schedule(self =>
+                {
+                    var currentValue = Console.ReadLine();
+                    double value;
+
+                    if (double.TryParse(currentValue, out value))
+                    {
+                        observer.OnNext(value);
+                    }
+
+                    self();
+                });
+            }).Publish();
         }
     }
 }
